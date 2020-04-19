2a9c75ea0117e2c9d659946b358ed915b5069937
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-19 20:00:08 +0530

    Update Finding relation between 2 arrays.py

--- Finding relation between 2 arrays.py
+++ Finding relation between 2 arrays.py
@@ -2,10 +2,11 @@
 # xs = [-1, 0.0, 1.0, 2.0, 3.0, 4.0]
 # ys = [-3.0, -1.0, 1.0, 3.0, 5.0, 7.0]
 
-
+#importing the neccessary packages
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
+
 
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
